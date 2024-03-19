35dfe8039cdbdfa1c593b1d5bbe3f1ac56ba6193
TMaxElectronics <60486146+TMaxElectronics@users.noreply.github.com>
2024-03-19 03:27:18 +0100

    Made it actually usable, added NULL module support

--- PWM.c
+++ PWM.c
@@ -6,109 +6,201 @@
 
 static uint32_t TxDIVIDERS[] = {1, 2, 4, 8, 16, 32, 64, 256};
 
-static void PWM_setDutyRegister(uint32_t module, uint32_t value);
-static uint32_t PWM_getDutyRegister(uint32_t module);
-static void PWM_clearControlRegister(uint32_t module, uint32_t value);
-static void PWM_setControlRegister(uint32_t module, uint32_t value);
-static void PWM_writeControlRegister(uint32_t module, uint32_t value);
-static uint32_t PWM_getControlRegister(uint32_t module);
-static uint32_t PWM_getTimerCompareValue(uint32_t module);
+#define TCON *(source->CON)
+#define TCONbits (*source->CON)
+#define TCONSET (*source->CONCLR)
+#define TCONCLR (*source->CONSET)
 
+#define PR (*source->TPR)
+#define PWMPR (*module->clockSource->TPR)
+#define TMR (*source->COUNTREG)
 
+#define OCCON module->CON->w
+#define OCCONbits (*module->CON)
+#define OCCONSET (*module->CONSET)
+#define OCCONCLR (*module->CONCLR)
 
+#define OCRS (*module->RS)
 
-void PWM_initClockSource(uint32_t source, uint32_t frequency){
-	//initialize a timer as a pwm clock source. Which ones are available depends on the device used
-
-	switch(source){
+static uint32_t PWM_populateSourceRegs(PWM_ClockSource_t * source, uint32_t timerID){
+	switch(timerID){
 		case 2:
-			//first check if the selected timer is not already active
-			if(T2CONbits.ON){
-				//damn it is... assert and return without modifying any settings
-				configASSERT(0);
-				return;
-			}
-			
-			//set the selected frequency
-			PWM_setFrequency(source, frequency);
-			
-			//enable the timer
-			T2CONbits.ON = 1;
-			
+            source->CON = &T2CON;
+            source->CONCLR = &T2CONCLR;
+            source->CONSET = &T2CONSET;
+            
+            source->TPR = &PR2;
+            source->COUNTREG = &TMR2;
+            
+            source->pwmSourceSelectValue = 0;
 			break;
 			
 		case 3:
-			//first check if the selected timer is not already active
-			if(T3CONbits.ON){
-				//damn it is... assert and return without modifying any settings
-				configASSERT(0);
-				return;
-			}
-			
-			//set the selected frequency
-			PWM_setFrequency(source, frequency);
-			
-			//enable the timer
-			T2CONbits.ON = 1;
-			
+            source->CON = &T3CON;
+            source->CONCLR = &T3CONCLR;
+            source->CONSET = &T3CONSET;
+            
+            source->TPR = &PR3;
+            source->COUNTREG = &TMR3;
+            
+            source->pwmSourceSelectValue = 1;
 			break;
 			
 		default:
-			return;
+			return 0;
 	}
-}
-
-void PWM_initModule(uint32_t module, uint32_t timer, uint32_t * outputPinRPR){
-	//set module mode to PWM without fault input and enable the module
-	PWM_setControlRegister(module, _OC2CON_ON_MASK | 0b110 | ((timer == 3) ? _OC1CON_OCTSEL_MASK : 0));
-}
-
-void PWM_setDuty(uint32_t module, uint32_t duty){
-    if(duty > 0xffff) return;
-      
     
-    //calculate comparator value
-    uint32_t comp = (duty * PWM_getTimerCompareValue(module)) >> 16;
-	
-	PWM_setDutyRegister(module, comp);
+    source->timerID = timerID;
+    
+    return 1;
 }
 
-uint32_t PWM_getDuty(uint32_t module){
-    return (PWM_getDutyRegister(module) << 16) / PWM_getTimerCompareValue(module);
-}
-
-uint32_t PWM_isModuleOn(uint32_t module){
-	return (PWM_getControlRegister(module) & _OC1CON_ON_MASK) != 0;
-}
-
-void PWM_setModuleOn(uint32_t module, uint32_t on){
-	if(on){
-		PWM_setControlRegister(module, _OC1CON_ON_MASK);
-	}else{
-		PWM_clearControlRegister(module, _OC1CON_ON_MASK);
-	}
-}
-
-void PWM_setFrequency(uint32_t timer, uint32_t freq){
-	//is the selected timer even enabled? If not just return without doing anything
-	switch(timer){
+static uint32_t PWM_populateModuleRegs(PWM_Handle_t * module, PWM_ClockSource_t * source, uint32_t moduleID){
+	switch(moduleID){
+		case 1:
+            module->CON = &OC1CON;
+            module->CONCLR = &OC1CONCLR;
+            module->CONSET = &OC1CONSET;
+            module->RS = &OC1RS;
+            module->outputPinValue = 0b0101;
+			break;
+            
 		case 2:
-			if(!T2CONbits.ON) return;
+            module->CON = &OC2CON;
+            module->CONCLR = &OC2CONCLR;
+            module->CONSET = &OC2CONSET;
+            module->RS = &OC2RS;
+            module->outputPinValue = 0b0101;
+			break;
+            
 		case 3:
-			if(!T3CONbits.ON) return;
+            module->CON = &OC3CON;
+            module->CONCLR = &OC3CONCLR;
+            module->CONSET = &OC3CONSET;
+            module->RS = &OC3RS;
+            module->outputPinValue = 0b0101;
+			break;
+            
+		case 4:
+            module->CON = &OC4CON;
+            module->CONCLR = &OC4CONCLR;
+            module->CONSET = &OC4CONSET;
+            module->RS = &OC4RS;
+            module->outputPinValue = 0b0101;
+			break;
+            
+		case 5:
+            module->CON = &OC1CON;
+            module->CONCLR = &OC1CONCLR;
+            module->CONSET = &OC1CONSET;
+            module->RS = &OC1RS;
+            module->outputPinValue = 0b0110;
+			break;
+            
+		default:
+			return 0;
 	}
+
+    module->moduleID = moduleID;
+    module->clockSource = source;
+    
+    return 1;
+}
+
+PWM_ClockSource_t * PWM_initClockSource(uint32_t timerID, uint32_t frequency){
+    PWM_ClockSource_t * source = pvPortMalloc(sizeof(PWM_ClockSource_t));
+    
+    if(!PWM_populateSourceRegs(source, timerID)){
+        vPortFree(source);
+        return NULL;
+    }
+    
+    //first check if the selected timer is not already active
+    if(TCONbits.ON){
+        //meh its on, free memory and return NULL
+        vPortFree(source);
+        return NULL;
+    }
+    
+    //set the selected frequency
+    PWM_setFrequency(source, frequency);
+			
+    //enable the timer
+    TCONbits.ON = 1;
+    
+    return source;
+}
+
+PWM_Handle_t * PWM_initModule(uint32_t moduleID, PWM_ClockSource_t * timer, uint32_t * outputPinRPR){
+    //check for valid timer pointer
+    if(timer == NULL) return;
+    
+    PWM_Handle_t * module = pvPortMalloc(sizeof(PWM_Handle_t));
+    
+    if(!PWM_populateModuleRegs(module, timer, moduleID)){
+        vPortFree(module);
+        return NULL;
+    }
+    
+    //first check if the selected module is not already active
+    if(OCCONbits.ON){
+        //meh its on, free memory and return NULL
+        vPortFree(module);
+        return NULL;
+    }
+    
+    *outputPinRPR = module->outputPinValue;
+    
+	//set module mode to PWM without fault input and enable the module
+    OCCON = _OC2CON_ON_MASK | 0b110 | (timer->pwmSourceSelectValue ? _OC1CON_OCTSEL_MASK : 0);
+    
+    return module;
+}
+
+void PWM_setDuty(PWM_Handle_t * module, int32_t duty){
+    //check for valid module pointer
+    if(module == NULL) return;
+            
+    if(duty > 0xffff) duty = 0xffff;
+      
+    //calculate comparator value
+    uint32_t comp = (duty * PWMPR) >> 16;
 	
-    //limit max frequency
+	OCRS = comp;
+}
+
+uint32_t PWM_getDuty(PWM_Handle_t * module){
+    //check for valid module pointer
+    if(module == NULL) return 0;
+    
+    return (OCRS << 16) / PWMPR;
+}
+
+uint32_t PWM_isModuleOn(PWM_Handle_t * module){
+    //check for valid module pointer
+    if(module == NULL) return 0;
+    
+	return OCCON & _OC1CON_ON_MASK;
+}
+
+void PWM_setModuleOn(PWM_Handle_t * module, uint32_t on){
+    //check for valid module pointer
+    if(module == NULL) return;
+	if(on){
+		OCCONSET = _OC1CON_ON_MASK;
+	}else{
+		OCCONCLR = _OC1CON_ON_MASK;
+	}
+}
+
+void PWM_setFrequency(PWM_ClockSource_t * source, uint32_t freq){
+    //check for valid source pointer
+    if(source == NULL) return;
+    
+    //limit max frequency TODO add sensible limits
     if(freq > 250000 || freq < 15) return;
 	
-    //first make sure all outputs running from this timer are off
-	uint32_t moduleEnables[PWM_MODULECOUNT] = {[0 ... (PWM_MODULECOUNT - 1)] = 0};
-	for(uint32_t i = 0; i < PWM_MODULECOUNT; i++){
-		if(timer == (((PWM_getControlRegister(i + 1) & _OC1CON_OCTSEL_MASK) >> 3) + 2)){
-			moduleEnables[i] = PWM_isModuleOn(i + 1);
-			PWM_setModuleOn(i + 1, 0);
-		}
-	}
+    //first make sure all outputs running from this timer are off TODO do we actually need this? If so: re-implement
 
     //find best match divider
     uint32_t currDividerIndex = 0;
@@ -122,163 +214,15 @@ void PWM_setFrequency(uint32_t timer, uint32_t freq){
     }
     
     //update timer period value and divider
-	switch(timer){
-		case 2:
-			T2CONbits.TCKPS = currDividerIndex;
-			PR2 = configPERIPHERAL_CLOCK_HZ / (TxDIVIDERS[currDividerIndex] * freq);
-			break;
-		case 3:
-			T3CONbits.TCKPS = currDividerIndex;
-			PR3 = configPERIPHERAL_CLOCK_HZ / (TxDIVIDERS[currDividerIndex] * freq);
-			break;
-	}
+	TCONbits.TCKPS = currDividerIndex;
+    PR = configPERIPHERAL_CLOCK_HZ / (TxDIVIDERS[currDividerIndex] * freq);
     
-	//finally re-enable all the modules that were enabled before
-	for(uint32_t i = 0; i < PWM_MODULECOUNT; i++){
-		if(moduleEnables[i]) PWM_setModuleOn(i + 1, 1);
-	}
+	//finally re-enable all the modules that were enabled before TODO
 }
 
-uint32_t PWM_getFreq(uint32_t timer){
-	switch(timer){
-		case 2:
-			return configPERIPHERAL_CLOCK_HZ / (TxDIVIDERS[T2CONbits.TCKPS] * PR2);
-		case 3:
-			return configPERIPHERAL_CLOCK_HZ / (TxDIVIDERS[T3CONbits.TCKPS] * PR3);
-	}
-}
-
-
-
-//Hardware access functions
-static uint32_t PWM_getTimerCompareValue(uint32_t module){
-	return (PWM_getControlRegister(module) & _OC1CON_OCTSEL_MASK) ? PR3 : PR2;
-}
-
-static uint32_t PWM_getControlRegister(uint32_t module){
-	switch(module){
-		case 1:
-			return OC1CON;
-		case 2:
-			return OC2CON;
-		case 3:
-			return OC3CON;
-		case 4:
-			return OC4CON;
-		case 5:
-			return OC5CON;
-			
-		default:
-			return 0;
-	}
-}
-
-static void PWM_writeControlRegister(uint32_t module, uint32_t value){
-	switch(module){
-		case 1:
-			OC1CON = value;
-			return;
-		case 2:
-			OC2CON = value;
-			return;
-		case 3:
-			OC3CON = value;
-			return;
-		case 4:
-			OC4CON = value;
-			return;
-		case 5:
-			OC5CON = value;
-			return;
-			
-		default:
-			return;
-	}
-}
-
-static void PWM_setControlRegister(uint32_t module, uint32_t value){
-	switch(module){
-		case 1:
-			OC1CONSET = value;
-			return;
-		case 2:
-			OC2CONSET = value;
-			return;
-		case 3:
-			OC3CONSET = value;
-			return;
-		case 4:
-			OC4CONSET = value;
-			return;
-		case 5:
-			OC5CONSET = value;
-			return;
-			
-		default:
-			return;
-	}
-}
-
-static void PWM_clearControlRegister(uint32_t module, uint32_t value){
-	switch(module){
-		case 1:
-			OC1CONCLR = value;
-			return;
-		case 2:
-			OC2CONCLR = value;
-			return;
-		case 3:
-			OC3CONCLR = value;
-			return;
-		case 4:
-			OC4CONCLR = value;
-			return;
-		case 5:
-			OC5CONCLR = value;
-			return;
-			
-		default:
-			return;
-	}
-}
-
-static uint32_t PWM_getDutyRegister(uint32_t module){
-	switch(module){
-		case 1:
-			return OC1RS;
-		case 2:
-			return OC2RS;
-		case 3:
-			return OC3RS;
-		case 4:
-			return OC4RS;
-		case 5:
-			return OC5RS;
-			
-		default:
-			return 0;
-	}
-}
-
-static void PWM_setDutyRegister(uint32_t module, uint32_t value){
-	switch(module){
-		case 1:
-			OC1RS = value;
-			return;
-		case 2:
-			OC2RS = value;
-			return;
-		case 3:
-			OC3RS = value;
-			return;
-		case 4:
-			OC4RS = value;
-			return;
-		case 5:
-			OC5RS = value;
-			return;
-			
-		default:
-			return;
-	}
+uint32_t PWM_getFreq(PWM_ClockSource_t * source){
+    //check for valid source pointer
+    if(source == NULL) return 0;
+    
+	return configPERIPHERAL_CLOCK_HZ / (TxDIVIDERS[T2CONbits.TCKPS] * PR);
 }
--- include/PWM.h
+++ include/PWM.h
@@ -1,15 +1,113 @@
 #ifndef PWM_INC
 #define PWM_INC
 
-#define PWM_MODULECOUNT 5
+typedef union {
+  struct {
+    uint32_t OCM:3;
+    uint32_t OCTSEL:1;
+    uint32_t OCFLT:1;
+    uint32_t OC32:1;
+    uint32_t :7;
+    uint32_t SIDL:1;
+    uint32_t :1;
+    uint32_t ON:1;
+  };
+  struct {
+    uint32_t OCM0:1;
+    uint32_t OCM1:1;
+    uint32_t OCM2:1;
+  };
+  struct {
+    uint32_t :13;
+    uint32_t OCSIDL:1;
+  };
+  struct {
+    uint32_t w:32;
+  };
+} OCxCONbits_t;
 
-void PWM_initClockSource(uint32_t source, uint32_t frequency);
-void PWM_initModule(uint32_t module, uint32_t timer, uint32_t * outputPinRPR);
-void PWM_setDuty(uint32_t module, uint32_t duty);
-uint32_t PWM_getDuty(uint32_t module);
-uint32_t PWM_isModuleOn(uint32_t module);
-void PWM_setModuleOn(uint32_t module, uint32_t on);
-void PWM_setFrequency(uint32_t timer, uint32_t freq);
-uint32_t PWM_getFreq(uint32_t timer);
+typedef union {
+  struct {
+    uint32_t :1;
+    uint32_t TCS:1;
+    uint32_t :2;
+    uint32_t TCKPS:3;
+    uint32_t TGATE:1;
+    uint32_t :5;
+    uint32_t SIDL:1;
+    uint32_t :1;
+    uint32_t ON:1;
+  };
+  struct {
+    uint32_t :4;
+    uint32_t TCKPS0:1;
+    uint32_t TCKPS1:1;
+    uint32_t TCKPS2:1;
+  };
+  struct {
+    uint32_t :13;
+    uint32_t TSIDL:1;
+    uint32_t :1;
+    uint32_t TON:1;
+  };
+  struct {
+    uint32_t w:32;
+  };
+} TxCONbits_t;
+
+typedef struct{
+    volatile TxCONbits_t    *   CON;
+    volatile uint32_t       *   CONCLR;
+    volatile uint32_t       *   CONSET;
+    
+    volatile uint32_t       *   TPR;
+    volatile uint32_t       *   COUNTREG;
+    
+    uint32_t timerID;
+    uint32_t pwmSourceSelectValue;
+} PWM_ClockSource_t;
+
+typedef struct{
+    volatile OCxCONbits_t   *   CON;
+    volatile uint32_t       *   CONCLR;
+    volatile uint32_t       *   CONSET;
+    
+    volatile uint32_t       *   RS;
+    
+    uint32_t moduleID;
+    
+    uint32_t outputPinValue;
+    
+    PWM_ClockSource_t * clockSource;
+} PWM_Handle_t;
+
+PWM_ClockSource_t * PWM_initClockSource(uint32_t timerID, uint32_t frequency);
+PWM_Handle_t * PWM_initModule(uint32_t moduleID, PWM_ClockSource_t * timer, uint32_t * outputPinRPR);
+void PWM_setDuty(PWM_Handle_t * module, int32_t duty);
+uint32_t PWM_getDuty(PWM_Handle_t * module);
+uint32_t PWM_isModuleOn(PWM_Handle_t * module);
+void PWM_setModuleOn(PWM_Handle_t * module, uint32_t on);
+void PWM_setFrequency(PWM_ClockSource_t * source, uint32_t freq);
+uint32_t PWM_getFreq(PWM_ClockSource_t * source);
+
+#if defined(OC8CON)
+#define PWM_MODULECOUNT 8
+#elif defined(OC7CON)
+#define PWM_MODULECOUNT 7
+#elif defined(OC6CON)
+#define PWM_MODULECOUNT 6
+#elif defined(OC5CON)
+#define PWM_MODULECOUNT 5
+#elif defined(OC4CON)
+#define PWM_MODULECOUNT 4
+#elif defined(OC3CON)
+#define PWM_MODULECOUNT 3
+#elif defined(OC2CON)
+#define PWM_MODULECOUNT 2
+#elif defined(OC1CON)
+#define PWM_MODULECOUNT 1
+#else
+#error No PWM Channels available on this device!
+#endif
 
 #endif
